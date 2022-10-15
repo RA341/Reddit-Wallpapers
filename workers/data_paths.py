65c0a381ed8a04a63e41f9bee82367f320e6b7d8
RA341 <razerishi1505@gmail.com>
2022-10-15 13:11:10 +0530

    refactored data paths

---
+++ workers/data_paths.py
@@ -0,0 +1,7 @@
+# Worker folder paths
+# WARNING DO NOT CHANGE THESE UNLESS YOU KNOW WHAT YOU ARE DOING
+data_folder = './lists'
+subreddits_file = '../subreddits.txt'
+old_wallpaper_list = './lists/downloaded_wallpaper.pickle'
+token_path = '../lists/refresh_token.pickle'
+
