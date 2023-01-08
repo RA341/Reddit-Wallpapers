ff112da9d8c6e51d01f5f5f1c0b53d704022baa8
ra341 <razerishi1505@gmail.com>
2023-01-08 16:09:18 +0530

    fixed authenticate

--- src/setup.py
+++ src/setup.py
@@ -43,7 +43,7 @@ class Setup:
             with open(self.config_path, 'w') as outfile:
                 json.dump(data, outfile)
 
-        if not os.path.exists(self.image_history_path):
+        if os.path.exists(self.image_history_path) is False:
             data = {}  # input an empty list
             with open(self.image_history_path, 'w') as outfile:
                 json.dump(data, outfile)
@@ -51,20 +51,20 @@ class Setup:
         print("Created required folders\n")
 
     def authenticate(self):
+        if self.settings['refresh_token'] is None:
+            # enter the client details
+            self.settings['client_id'] = input('Enter client id (found on https://www.reddit.com/prefs/apps)\n:')
+            self.settings['client_secret'] = input(
+                'Enter client secret (found on https://www.reddit.com/prefs/apps)\n:')
 
-        # enter the client details
-        self.settings['client_id'] = input('Enter client id (found on https://www.reddit.com/prefs/apps)\n:')
-        self.settings['client_secret'] = input(
-            'Enter client secret (found on https://www.reddit.com/prefs/apps)\n:')
-
-        tmp = reddit_login(self.settings['client_id'],
-                           self.settings['client_secret'])  # authorise using reddit_auth.py
-        if tmp != 1:  # check if function returns 1(error) or token
-            self.settings['refresh_token'] = tmp
-            self.file_obj.seek(0)  # move cursor to start of the file for overwriting else appends it to end of file
-            json.dump(self.settings, self.file_obj)  # write to file
-            self.file_obj.close()
-        else:
-            print("Could not get reddit login token")
-            print("Exiting")
-            quit(-1)
+            tmp = reddit_login(self.settings['client_id'],
+                               self.settings['client_secret'])  # authorise using reddit_auth.py
+            if tmp != 1:  # check if function returns 1(error) or token
+                self.settings['refresh_token'] = tmp
+                self.file_obj.seek(0)  # move cursor to start of the file for overwriting else appends it to end of file
+                json.dump(self.settings, self.file_obj)  # write to file
+                self.file_obj.close()
+            else:
+                print("Could not get reddit login token")
+                print("Exiting")
+                quit(-1)
